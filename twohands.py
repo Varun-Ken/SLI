6001928d72674a9af394649fb87feea350cee016
Varun <76640796+Varun-Ken@users.noreply.github.com>
2023-04-30 16:40:04 +0530

    Create twohands.py

---
+++ twohands.py
@@ -0,0 +1,129 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import tkinter as tk
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=2)
+classifier = Classifier("Model3/keras_model.h5", "Model3/labels.txt")
+
+offset = 20
+imgSize = 300
+
+folder = "Data/"
+counter = 0
+
+labels = ["Home","Good","Help","How","Name"]
+
+# Create a new Tkinter window
+root = tk.Tk()
+root.geometry("300x300")
+
+# Create a label widget to display the recognized text
+text_label = tk.Label(root, text="", font=("Helvetica", 48))
+text_label.pack(fill="both", expand=True)
+
+while True:
+    success, img = cap.read()
+    imgOutput = img.copy()
+    hands, img = detector.findHands(img)
+
+    if hands and len(hands) == 2:
+        x1, y1, w1, h1 = hands[0]['bbox']
+        x2, y2, w2, h2 = hands[1]['bbox']
+
+        # Calculate the bounding box for both hands
+        x, y, w, h = min(x1, x2), min(y1, y2), max(x1 + w1, x2 + w2) - min(x1, x2), max(y1 + h1, y2 + h2) - min(y1, y2)
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            text_label.config(text=labels[index])
+
+        cv2.rectangle(imgOutput, (x - offset, y - offset-50),
+                      (x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.rectangle(imgOutput, (x-offset, y-offset),
+                      (x + w+offset, y + h+offset), (255, 0, 255), 4)
+
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+    elif hands and len(hands) == 1:
+        success, img = cap.read()
+        imgOutput = img.copy()
+        hands, img = detector.findHands(img)
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
+
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
+            imgCropShape = imgCrop.shape
+
+            aspectRatio = h / w
+
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((imgSize - wCal) / 2)
+                imgWhite[:, wGap:wCal + wGap] = imgResize
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+                print(prediction, index)
+
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+                text_label.config(text=labels[index])
+                
+            cv2.rectangle(imgOutput, (x - offset, y - offset - 50),
+                          (x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
+            cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+            cv2.rectangle(imgOutput, (x - offset, y - offset),
+                          (x + w + offset, y + h + offset), (255, 0, 255), 4)
+
+            cv2.imshow("ImageCrop", imgCrop)
+            cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", imgOutput)
+    root.update()  # Update the Tkinter window
+
+    # Exit the loop and close the window when the "x" button is clicked
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        root.destroy()
+        break
+
+cap.release()
+cv2.destroyAllWindows()
